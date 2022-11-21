fbb77c630b442fe323b2dde1a32bbbb9d2fcdcfc
Nandini Rao [nandinirao] <nandinirao@costco.com>
2022-11-20 19:57:21 -0800

    classroom exercises

--- accessing.js
+++ accessing.js
@@ -1,5 +1,18 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
 
-// Change the styling of every element with class "sun" to set the color to "orange"
+const header = document.getElementById('weather-head');
+header.innerText = 'February 10 Weather Forecast, Seattle';
 
+// Change the styling of every element with class "sun" to set the color to "orange"
+const sunny = document.querySelectorAll('.sun');
+sunny.forEach(element => {
+    element.style.color = 'orange';
+});
+// const sunny = document.getElementsByClassName('sun');
+// for(let sun of sunny) {
+//     sun.style.color = 'blue';
+// }
 // Change the class of the second <li> from to "sun" to "cloudy"
+
+const li = document.querySelectorAll('li');
+li[1].className = 'cloudy';
--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,26 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
 
+const newAElement = document.createElement('a');
+newAElement.setAttribute('id', 'cta');
+const newText = document.createTextNode('Buy Now!');
+newAElement.appendChild(newText);
+
+const pElements = document.getElementsByTagName('p');
+const lastP = pElements[pElements.length-1];
+lastP.parentNode.appendChild(newAElement);
+
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
-
+console.log(document.getElementsByTagName('img')[0].dataset.color);
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+const liElement = document.getElementsByTagName('li')[2];
+liElement.className = 'highlight'
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+
+lastP.parentNode.removeChild(lastP);
