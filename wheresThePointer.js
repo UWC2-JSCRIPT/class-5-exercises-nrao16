0c61e99f0a32c39a3ce79502c3bace05dc87bce1
Nandini Rao [nandinirao] <nandinirao@costco.com>
2022-11-20 21:05:12 -0800

    pointer hw

--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,13 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+
+const tdList = document.getElementsByTagName('table');
+
+const onClickHandler = (e) => {
+    if(e.target.tagName != 'TD') return;
+
+    e.target.innerHTML =`${e.clientX}, ${e.clientY}`;
+}
+
+tdList[0].addEventListener('click', onClickHandler);
